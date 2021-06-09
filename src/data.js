f4f8a74e23f45abd22b6b3b7c3ed1da8b490d324
ShahbajKhan <shktoky@gmail.com>
2021-06-09 23:15:24 +0600

    my first react project with tailwind css

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    path: 'https://i.ibb.co/Jv5kRr7/image.png',
+    desc: 'Mama Jaben?-car rental',
+  },
+  {
+    id: 2,
+    path: 'https://i.ibb.co/zVHkJpC/image.png',
+    desc: 'Evergreen Shop',
+  },
+  {
+    id: 3,
+    path: 'https://i.ibb.co/Bn3NzsM/fair-fares-web-app.png',
+    desc: 'Fair Fares',
+  },
+  {
+    id: 4,
+    path: './images/huddle-landing-page.png',
+    desc: 'Huddle Landing Page',
+  },
+  {
+    id: 5,
+    path: './images/insure-landing-page.png',
+    desc: 'Insure Landing Page',
+  },
+  {
+    id: 6,
+    path: './images/intro-component.png',
+    desc: 'Intro Component',
+  },
+]
+
+export default data
